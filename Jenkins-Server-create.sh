d48cb5989adbda9a244bc4c4b77b93f52607c221
asasank <sonthu.asa@gmail.com>
2020-06-24 00:10:31 -0500

    Update Jenkins-Server-create.sh

--- Jenkins-Server-create.sh
+++ Jenkins-Server-create.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 #Essential Services Installation
-
-yum install java python38 gcc wget tar git -y 
+yum update -y
+yum install java python38 gcc wget tar git docker yum-utils -y 
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 #AWS CLI Installation 
-
 curl -O https://bootstrap.pypa.io/get-pip.py
- python3.8 get-pip.py --user
- export PATH=~/.local/bin:$PATH
- source ~/.bash_profile
- pip3 install awscli --upgrade --user
+python3.8 get-pip.py --user
+export PATH=~/.local/bin:$PATH
+source ~/.bash_profile
+pip3 install awscli --upgrade --user
 
 #Jenkins Installation
-
- wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
-
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
-
 yum install jenkins -y 
-
 systemctl start jenkins
-
 systemctl enable jenkins
